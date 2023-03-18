8fe1fa7c4f22c4a458ac946a0f493207c2777ddf
userkilled <39238592+userkilled@users.noreply.github.com>
2023-03-18 12:28:03 +0300

    Update Link.java
    testing remove google app links button

--- app/src/main/java/tw/com/daxia/virtualsoftkeys/common/Link.java
+++ app/src/main/java/tw/com/daxia/virtualsoftkeys/common/Link.java
@@ -6,8 +6,8 @@ package tw.com.daxia.virtualsoftkeys.common;
 
 public interface Link {
 
-    String GOOGLE_APP_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
-    String GOOGLE_PLAY_LINK = "http://play.google.com/store/apps/details?id=";
+    //String GOOGLE_APP_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
+    //String GOOGLE_PLAY_LINK = "http://play.google.com/store/apps/details?id=";
     String MY_GIT_HUB_URL = "https://github.com/DaxiaK/VirtualSoftKeys";
 
 }
